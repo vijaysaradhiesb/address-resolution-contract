ceb55171020df234be061010e81b3dccf98141b7
Piotr KLimczak <piotr.klimczak@hubio.com>
2017-10-31 16:20:55 +0000

    ICE-4846 JSON exception handling improvements

--- src/main/java/com/hubio/integration/services/address/api/AddressLookupService.java
+++ src/main/java/com/hubio/integration/services/address/api/AddressLookupService.java
@@ -1,5 +1,6 @@
 package com.hubio.integration.services.address.api;
 
+import com.hubio.integration.services.contracts.common.GenericInternalServerException;
 import org.apache.camel.Header;
 
 import javax.ws.rs.*;
@@ -12,21 +13,21 @@ public interface AddressLookupService {
     @Path("/search")
     @Consumes({MediaType.TEXT_XML})
     @Produces({MediaType.TEXT_XML})
-    AbstractSearchAddressResponse search(SearchRequest searchRequest);
+    SearchResponse search(SearchRequest searchRequest) throws BadRequestException, TimedOutException, GenericInternalServerException;
 
     @POST
     @Path("/resolve")
     @Consumes({MediaType.TEXT_XML})
     @Produces({MediaType.TEXT_XML})
-    AbstractResolveAddressResponse resolve(ResolveRequest resolveRequest);
+    ResolutionResponse resolve(ResolveRequest resolveRequest) throws BadRequestException, TimedOutException, AddressIdNotFoundException, GenericInternalServerException;
 
     @GET
     @Path("/search/{countryCode}")
     @Produces({MediaType.APPLICATION_JSON})
-    AbstractSearchAddressResponse search(@PathParam("countryCode") String countryCode, @QueryParam("searchTerm") String searchTerm, @QueryParam("partialAddressId") String partialAddressId);
+    SearchResponse search(@PathParam("countryCode") String countryCode, @QueryParam("searchTerm") String searchTerm, @QueryParam("partialAddressId") String partialAddressId) throws BadRequestException, TimedOutException;
 
     @GET
     @Path("/resolve")
     @Produces({MediaType.APPLICATION_JSON})
-    AbstractResolveAddressResponse resolve(@QueryParam("addressId") @Header("addressId") String addressId);
+    ResolutionResponse resolve(@QueryParam("addressId") @Header("addressId") String addressId) throws BadRequestException, TimedOutException, AddressIdNotFoundException;
 }
